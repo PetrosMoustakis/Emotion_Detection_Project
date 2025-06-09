567087ca901d3daa6db55b69add567da3e868aa7
Petros Moustakis <petrosmoukas16@gmail.com>
2025-06-09 13:47:59 +0300

    Fix file paths in config.py for portability

--- src/config.py
+++ src/config.py
@@ -1,5 +1,9 @@
 import os
 
-train_dir = '..\\data\\train'
-test_dir = '..\\data\\test'
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+train_dir = os.path.join(PROJECT_ROOT, 'data', 'train')
+test_dir = os.path.join(PROJECT_ROOT, 'data', 'test')
 image_classes = os.listdir(train_dir)
+
+
